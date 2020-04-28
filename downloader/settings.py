ccedacda4fe6148b15d50e2d0c6f88ff1dc87865
jjvera96 <jjvera96@gmail.com>
2020-04-28 11:43:30 -0500

    Environment and change in titles

--- downloader/settings.py
+++ downloader/settings.py
@@ -19,7 +19,7 @@ import environ
 root = environ.Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 env = environ.Env(DEBUG=(bool, False), )
 environ.Env.read_env()
-SITE_ROOT = root()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
